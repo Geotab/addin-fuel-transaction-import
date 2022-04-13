a75fd7a7062f5637c50ca09696d69b750baa9557
giolo11geo <fabiopolini@geotab.com>
2022-04-13 18:25:33 +0200

    aet null cost when cell is empty

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1576,6 +1576,11 @@ geotab.addin.addinFuelTransactionImport_fp = function () {
                         newTranscationObj[prop] = parseFloat(singleTransaction[provider[prop]].replace(/,/g, '.'));
 
                     }
+                    ///// same issue as https://jira.geotab.com/browse/EU-2625
+                    else {
+                        if (singleTransaction[provider[prop]] == "") newTranscationObj[prop] = null;
+                    }
+                    ///// same issue as https://jira.geotab.com/browse/EU-2625
 
 
                     break;
@@ -1730,6 +1735,11 @@ geotab.addin.addinFuelTransactionImport_fp = function () {
                             newTranscationObj[prop] = parseFloat(tmp).toFixed(1);
                         }
                     }
+                    ///// same as https://jira.geotab.com/browse/EU-2625
+                    else {
+                        if (singleTransaction[provider[prop]] == "") newTranscationObj[prop] = null;
+                    }
+                    ///// same as https://jira.geotab.com/browse/EU-2625
                     break;
 
                 default:
@@ -2081,6 +2091,7 @@ geotab.addin.addinFuelTransactionImport_fp = function () {
 
             console.log("offset In Number taken from picker: ", offsetInNumber);
             console.log("Date not formatted: ", date);
+            console.log("Format Found ",formatFound);
             dateFormatted = moment.utc(date, formatFound, true).utcOffset(offsetInNumber, true).format();
 
             console.log("dateFormatted: ", dateFormatted);
