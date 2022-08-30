c7240bb2fa9949d7b02b95dc47e3045851dd6641
Brett Kelley <brettkelley@geotab.com>
2022-08-30 16:43:02 +0200

    work on transaction parsing

--- ftiAddin/src/app/scripts/TransactionHelper.js
+++ ftiAddin/src/app/scripts/TransactionHelper.js
@@ -2,19 +2,22 @@ function ParseAndBuildTransactions(transactionsExcel, configuration) {
     return new Promise((resolve, reject) => {
         // var data = JSON.stringify(transactionsExcel);
         // data = JSON.parse(data);
+        let transactionsOutput = [];
         transactionsExcel.forEach((transaction, i) => {
             if (i === 0) {
                 // title row so ignore
             } else {
-                parseTransaction(transaction, configuration);
+                transactionsOutput.push(parseTransaction(transaction, configuration));
             }
         });
+        console.log('transactionsOutput: ' + JSON.stringify(transactionsOutput));
         resolve('ParseAndBuildTransactions completed.')
     });
 }
 
 function parseTransaction(transaction, configuration) {
     let transOutput;
+    let entity = {};
     let unitVolumeLiters = configuration.unitVolumeLiters;
     let unitOdoKm = configuration.unitOdoKm;
     let isCellDateType = configuration.isCellDateType;
@@ -27,12 +30,16 @@ function parseTransaction(transaction, configuration) {
     // loop through the data properties of the transaction object
     // key = property
     Object.keys(configuration.data).forEach(key => {
-        console.log(key, configuration[key]);
+        console.log(key, configuration.data[key]);
+        entity[key] = transaction[configuration.data[key]];
     });
 
-    // if (transaction['cardNumber']){
-    //     console
-    // }
+    console.log(configuration.Name, JSON.stringify(entity));
+    return entity;
+}
+
+function processTransactionProperty(transaction, configuration){
+
 }
 
 module.exports = {
