962c1411d317c205f9014a2bbf1a1b2b2a1a6ecd
Brett Kelley <brettkelley@geotab.com>
2023-03-08 18:47:22 +0100

    added getISODateAdjusted

--- ftiAddin/src/app/scripts/DateHelper.js
+++ ftiAddin/src/app/scripts/DateHelper.js
@@ -15,6 +15,22 @@ function getDateAdjusted(inputDate, remoteZone, localZone) {
    return localDate.toJSDate();
 }
 
+/**
+ * Gets a local javascript date adjusted from a remotely created one.
+ * @param {Object} inputDateISO An ISO 8601 formatted string.
+ * @param {String} remoteZone The remote time zone string. e.g. Europe/Zurich
+ * @param {String} localZone The local time zone string.
+ * @returns A javascript date object containing the local representation of the input date.
+ */
+function getISODateAdjusted(inputDateISO, remoteZone, localZone) {
+   const remoteDate = DateTime.fromISO(inputDateISO.slice(0, -1), { zone: remoteZone });
+   console.log(`remoteDate: ${remoteDate.toString()}`);
+   const localDate = remoteDate.setZone(localZone);
+   console.log(`localDate: ${localDate.toString()}`);
+   return localDate.toJSDate();
+}
+
 module.exports = {
-   getDateAdjusted
+   getDateAdjusted,
+   getISODateAdjusted
 }
