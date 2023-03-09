c158d36747c42051707918a7ecab70c72b5a3344
Brett Kelley <brettkelley@geotab.com>
2023-03-09 10:52:43 +0100

    DateHelper and tests

--- ftiAddin/src/app/scripts/DateHelper.js
+++ ftiAddin/src/app/scripts/DateHelper.js
@@ -1,4 +1,45 @@
+'use strict';
+
 const { DateTime } = require("luxon");
+
+class DateError extends Error {
+   constructor(message) {
+     super(message);
+   }
+}
+
+/**
+ * Formats a date/time unit to 2 places if only a single value.
+ * @param {*} unit The date/time unit to format.
+ * @returns A double digit date/time unit.
+ */
+function formatDateUnit(unit) {
+   let output = unit.toString();
+   if (output.length === 1) {
+      output = '0' + output;
+   }
+   // console.log(output);
+   return output;
+}
+
+/**
+ * Formats the JavaScript date in the ISO 8601 format (less the Z) e.g. 2017-07-15T14:00:00.000
+ * @param {*} date The date to format
+ * @returns An ISO 8601 formatted date e.g. 2017-07-15T14:00:00.000
+ */
+function getISODateFormat(date) {
+   const myDate = 
+      date.getFullYear() + '-' 
+      + formatDateUnit(date.getMonth() + 1) + '-' 
+      + formatDateUnit(date.getUTCDate());
+   const myTime = 
+      formatDateUnit(date.getHours()) + ':' 
+      + formatDateUnit(date.getMinutes()) + ':' 
+      + formatDateUnit(date.getSeconds()) + '.000';
+   const isoDate = myDate + 'T' + myTime;
+   console.log(`isoDate ${isoDate}`);
+   return isoDate;
+}
 
 /**
  * Gets a local javascript date adjusted from a remotely created one.
@@ -8,7 +49,12 @@ const { DateTime } = require("luxon");
  * @returns A javascript date object containing the local representation of the input date.
  */
 function getDateAdjusted(inputDate, remoteZone, localZone) {
-   const remoteDate = DateTime.fromISO(inputDate.toISOString().slice(0, -1), { zone: remoteZone });
+   console.log(`inputDate toLocaleString: ${inputDate.toLocaleString()}`);
+   console.log(`inputDate toISOString: ${inputDate.toISOString()}`);
+   const isoDate = getISODateFormat(inputDate);
+   console.log(`isoDate: ${isoDate}`);
+   const remoteDate = DateTime.fromISO(isoDate, { zone: remoteZone });
+   //const remoteDate = DateTime.fromISO(inputDate.toISOString().slice(0, -1), { zone: remoteZone });
    console.log(`remoteDate: ${remoteDate.toString()}`);
    const localDate = remoteDate.setZone(localZone);
    console.log(`localDate: ${localDate.toString()}`);
@@ -16,21 +62,107 @@ function getDateAdjusted(inputDate, remoteZone, localZone) {
 }
 
 /**
- * Gets a local javascript date adjusted from a remotely created one.
- * @param {Object} inputDateISO An ISO 8601 formatted string.
- * @param {String} remoteZone The remote time zone string. e.g. Europe/Zurich
- * @param {String} localZone The local time zone string.
- * @returns A javascript date object containing the local representation of the input date.
+ * isEmpty check
+ * @param {*} value 
+ * @returns 
  */
-function getISODateAdjusted(inputDateISO, remoteZone, localZone) {
-   const remoteDate = DateTime.fromISO(inputDateISO.slice(0, -1), { zone: remoteZone });
-   console.log(`remoteDate: ${remoteDate.toString()}`);
-   const localDate = remoteDate.setZone(localZone);
-   console.log(`localDate: ${localDate.toString()}`);
-   return localDate.toJSDate();
+function isEmpty(value) {
+   return (value == null || value.length === 0);
+}
+
+/**
+ * Tests whether an input is a JavaScript date object
+ * @param {*} date The input date.
+ * @returns true if yes and false if no.
+ */
+function isDateObject(date) {
+   if (typeof (date) == 'object') {
+       return true;
+   }
+   return false;
+}
+
+/**
+ * The method combines two separate date and time values to produce a single date object;
+ * @param {*} date The date representation of the new date/time.
+ * @param {*} time The time representation of the new date/time.
+ * @returns A new date/time object combining the date and time inputs to a single date. If the inputs don't match any expected value a null is returned.
+ */
+function combineDateAndTime(date, time) {
+   // if the date and time are date objects
+   if ((isDateObject(date)) && (isDateObject(time))) {
+      const datetime = new Date(date.getFullYear(), date.getMonth(), date.getDate(), 
+                        time.getHours(), time.getMinutes(), time.getSeconds());
+      return datetime;
+   } else if ((isDateObject(date) == false) && (isDateObject(time) == false)) {
+       return date.trim() + ' ' + time.trim();
+   } else {
+      return null;
+   }
+}
+
+/**
+ * Using the Luxon library a JavaScript date is produced from a date string and it's relevant format.
+ * Uses the Luxon library implementation.
+ * @param {*} dateString 
+ * @param {*} format 
+ * @returns 
+ */
+function getJSDateFromString(dateString, format) {
+   const date = DateTime.fromFormat(dateString, format);
+   return date.toJSDate();
+}
+
+/**
+ * Gets a valid javascript date from the input data if possible
+ * @param {JSON} configuration JSON configuration.
+ * @param {*} inputDate The input date.
+ * @returns A JavaScript date Object
+ */
+function getJSDate(configuration, inputDate) {
+   
+   let output;
+   let date;
+   let dateFormat;
+
+   if (isEmpty(inputDate[0])) {
+      throw new DateError('No input date at inception of getJSDate.');
+   } else {
+       // date is populated
+       if (inputDate[1]) {
+           // time is populated
+           date = combineDateAndTime(inputDate[0], inputDate[1]);
+           if (isDateObject(date) === false)
+           {
+               // the date is a string object and therefore requires a format.
+               // Both date and time formats must be provided as the date and time are stored in separate columns.
+               if ((configuration.dateFormat) && (configuration.timeFormat)) {
+                     dateFormat = configuration.dateFormat + ' ' + configuration.timeFormat;
+               } else {
+                  // the time format has not been supplied and therefore an exception.
+                  return null;
+               }
+               // get the javascript date from the string format
+               output = getJSDateFromString(date, dateFormat);
+            } else {
+               output = date;
+            }
+       } else {
+         // time is NOT populated. date and time contained in a single cell.
+         date = inputDate[0];
+         if (isDateObject(date) === false)
+         {
+            dateFormat = configuration.dateFormat
+            output = getJSDateFromString(date, dateFormat);
+         } else {
+            output = date;
+         }
+       }
+   }
+   return output;
 }
 
 module.exports = {
    getDateAdjusted,
-   getISODateAdjusted
+   getJSDate
 }
--- ftiAddin/test/functional/DateHelperTests.js
+++ ftiAddin/test/functional/DateHelperTests.js
@@ -1,30 +1,152 @@
 const assert = require('chai').assert;
 const dateHelper = require('../../src/app/scripts/DateHelper');
 
+// getDateAdjusted tests
 describe('DateHelper Tests - getDateAdjusted tests', function(){
    it('getDateAdjusted - Dubai', function(){
-      let date = new Date('2017-07-15T16:00:00.000Z'); //+4
+      let date = new Date('2017-07-15T16:00:00.000'); //+4
       let resultDate = dateHelper.getDateAdjusted(date, 'Asia/Dubai', 'Europe/Zurich');
-      //console.log(resultDate.toISOString());
       assert.equal(resultDate.toISOString(), '2017-07-15T12:00:00.000Z');
    });
    it('getDateAdjusted - Cayman', function(){
-      let date = new Date('2017-07-15T16:00:00.000Z'); //-5
+      let date = new Date('2017-07-15T16:00:00.000'); //-5
       let resultDate = dateHelper.getDateAdjusted(date, 'America/Cayman', 'Europe/Zurich');
       //console.log(resultDate.toISOString());
       assert.equal(resultDate.toISOString(), '2017-07-15T21:00:00.000Z');
    });
    it('getDateAdjusted GMT', function(){
-      let date = new Date('2017-07-15T16:00:00.000Z');
+      let date = new Date('2017-07-15T16:00:00.000');
       let resultDate = dateHelper.getDateAdjusted(date, 'GMT', 'Europe/Zurich');
       //console.log(resultDate.toISOString());
       assert.equal(resultDate.toISOString(), '2017-07-15T16:00:00.000Z');
    });
    it('getDateAdjusted Local', function(){
-      let date = new Date('2017-07-15T16:00:00.000Z');
+      let date = new Date('2017-07-15T16:00:00.000');
       let resultDate = dateHelper.getDateAdjusted(date, 'Europe/Zurich', 'Europe/Zurich');
       //console.log(resultDate.toISOString());
       assert.equal(resultDate.toISOString(), '2017-07-15T14:00:00.000Z');
    });
 });
 
+// getJSDate tests
+describe('DateHelper Tests - getJSDate tests', function(){
+   let config = {};
+   let inputDate = [];
+   let testDate;
+   it('getJSDate - date object single', function(){
+      inputDate[0] = new Date('2022-04-16');
+      inputDate[1] = null;
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2022-04-16T00:00:00.000Z');
+   });
+   it('getJSDate - date object double', function(){
+      inputDate[0] = new Date('2022-04-16');
+      inputDate[1] = new Date('2022-04-16T12:00:00');
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2022-04-16T10:00:00.000Z');
+   });
+   it('getJSDate - date string - single - Germany', function(){
+      inputDate[0] = '2022-04-16';
+      inputDate[1] = null;
+      config.dateFormat = 'yyyy-MM-dd';
+      config.timeFormat = null;
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2022-04-15T22:00:00.000Z');
+   });
+   it('getJSDate - date string single long date', function(){
+      inputDate[0] = '04/16/2023 14:00';
+      inputDate[1] = null;
+      config.dateFormat = 'MM/dd/yyyy HH:mm';
+      config.timeFormat = null;
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2023-04-16T12:00:00.000Z');
+   });
+   it('getJSDate - date string double', function(){
+      inputDate[0] = '04/16/2023';
+      inputDate[1] = '14:00';
+      config.dateFormat = 'MM/dd/yyyy';
+      config.timeFormat = 'HH:mm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2023-04-16T12:00:00.000Z');
+   });
+   it('getJSDate - date string double 2', function(){
+      inputDate[0] = '04162023';
+      inputDate[1] = '1400';
+      config.dateFormat = 'MMddyyyy';
+      config.timeFormat = 'HHmm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2023-04-16T12:00:00.000Z');
+   });
+   it('getJSDate - date string double 2', function(){
+      inputDate[0] = '04162023';
+      inputDate[1] = '1400';
+      config.dateFormat = 'MMddyyyy';
+      config.timeFormat = 'HHmm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2023-04-16T12:00:00.000Z');
+   });
+   it('getJSDate - date string - single - French', function(){
+      inputDate[0] = '16/04/2022';
+      inputDate[1] = null;
+      config.dateFormat = 'dd/MM/yyyy';
+      config.timeFormat = null;
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2022-04-15T22:00:00.000Z');
+   });
+   it('getJSDate - date string - single - Italian', function(){
+      inputDate[0] = '16.04.22';
+      inputDate[1] = null;
+      config.dateFormat = 'dd.MM.yy';
+      config.timeFormat = null;
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2022-04-15T22:00:00.000Z');
+   });
+   // exceptions
+});
+
+// End to End tests
+describe('DateHelper Tests - E2E', function(){
+   let config = {};
+   let inputDate = [];
+   let testDate;
+   it('getJSDate - E2E - Dubai (+4) -> Zurich (+1)', function(){
+      inputDate[0] = '2017-07-15';
+      inputDate[1] = '16:00';
+      config.dateFormat = 'yyyy-MM-dd';
+      config.timeFormat = 'HH:mm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2017-07-15T14:00:00.000Z');
+      let resultDate = dateHelper.getDateAdjusted(testDate, 'Asia/Dubai', 'Europe/Zurich');
+      assert.equal(resultDate.toISOString(), '2017-07-15T12:00:00.000Z');
+   });
+   it('getJSDate - E2E - Cayman (-5) -> Zurich (+1)', function(){
+      inputDate[0] = '2017-07-15';
+      inputDate[1] = '16:00';
+      config.dateFormat = 'yyyy-MM-dd';
+      config.timeFormat = 'HH:mm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2017-07-15T14:00:00.000Z');
+      let resultDate = dateHelper.getDateAdjusted(testDate, 'America/Cayman', 'Europe/Zurich');
+      assert.equal(resultDate.toISOString(), '2017-07-15T21:00:00.000Z');
+   });
+   it('getJSDate - E2E - Zurich (+1) -> Zurich (+1)', function(){
+      inputDate[0] = '2017-07-15';
+      inputDate[1] = '16:00';
+      config.dateFormat = 'yyyy-MM-dd';
+      config.timeFormat = 'HH:mm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2017-07-15T14:00:00.000Z');
+      let resultDate = dateHelper.getDateAdjusted(testDate, 'Europe/Zurich', 'Europe/Zurich');
+      assert.equal(resultDate.toISOString(), '2017-07-15T14:00:00.000Z');
+   });
+   it('getJSDate - E2E - GMT (0) -> Zurich (+1)', function(){
+      inputDate[0] = '2017-07-15';
+      inputDate[1] = '16:00';
+      config.dateFormat = 'yyyy-MM-dd';
+      config.timeFormat = 'HH:mm';
+      testDate = dateHelper.getJSDate(config, inputDate);
+      assert.equal(testDate.toISOString(),'2017-07-15T14:00:00.000Z');
+      let resultDate = dateHelper.getDateAdjusted(testDate, 'GMT', 'Europe/Zurich');
+      assert.equal(resultDate.toISOString(), '2017-07-15T16:00:00.000Z');
+   });
+});
