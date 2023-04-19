38ec49e9deadcf3c3041fdca0f5d527a552d8a63
Brett Kelley <brettkelley@geotab.com>
2023-04-19 11:19:17 +0200

    config.json updates
    - added the name property update including the version.
    - added the SVG icon back and fixed the property.

--- ftiAddin/webpack.production.js
+++ ftiAddin/webpack.production.js
@@ -20,10 +20,12 @@ const transform = function (content, path) {
     let config = JSON.parse(content);
     let host = config.prod.dist.host;
     let len = config.items.length;
+    config.name = 'FTI V' + config.version;
     // Appending the host to all item's url and icon
     for(let i=0;i<len;i++){
         config.items[i].url = host + config.version + '/' + config.items[i].url;
-        config.items[i].icon = host + config.version + '/' + config.items[i].icon; 
+        // config.items[i].icon = host + config.version + '/' + config.items[i].icon; 
+        config.items[i].svgIcon = host + config.version + '/' + config.items[i].svgIcon; 
         config.items[i].menuName.en = config.prod.menuName.en + ' ' + config.version;
     }
 
